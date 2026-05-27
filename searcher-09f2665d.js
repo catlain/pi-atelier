44218878fe2232d4c61adf546bd751b4accfebbd
catlain <catlain@users.noreply.github.com>
2026-05-27 10:40:03 +0000

    deploy: 08626defb5dc8f5e5881fd33c905abc854cf683f

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-64b453cc.js',
+                path_to_root + 'searchindex-66e6d587.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
